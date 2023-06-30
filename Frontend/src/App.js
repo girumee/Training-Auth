985d0350885ea3ac1d70cb3cf9cee5b466cdb8aa
girumee <grmzegeye@gmail.com>
2023-06-30 17:32:10 +0300

    3rd push 30-06-2023

---
+++ Frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const App = () => (
+  <Router>
+    <Switch></Switch>
+  </Router>
+);
+
+export default App;
