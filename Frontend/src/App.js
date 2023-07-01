393620104d56c7810989e90475a17f63e0dfd887
girumee <grmzegeye@gmail.com>
2023-07-01 09:31:34 +0300

    Frontend page and backend connection on 01-07-2023

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,31 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./containers/Home";
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
+import ResetPassword from "./containers/ResetPassword";
+import ResetPasswordConfirm from "./containers/ResetPasswordConfirm";
+import Activate from "./containers/Activate";
+
+import Layout from "./hocs/Layout";
 
 const App = () => (
   <Router>
-    <Switch></Switch>
+    <Layout>
+      <Routes>
+        <Route exact path="/" Component={Home} />
+        <Route exact path="/login" Component={Login} />
+        <Route exact path="/signup" Component={Signup} />
+        <Route exact path="/reset-Password" Component={ResetPassword} />
+        <Route
+          exact
+          path="/password-reset/:uid/:token"
+          Component={ResetPasswordConfirm}
+        />
+        <Route exact path="/activate/:uid/:token" Component={Activate} />
+      </Routes>
+    </Layout>
   </Router>
 );
 
