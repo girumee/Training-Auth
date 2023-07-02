76eb0505084d6398dc4b5977ccbba52081a05012
girumee <grmzegeye@gmail.com>
2023-07-02 22:56:29 +0300

    login page finished and authentication 02-07-2023

--- Frontend/src/containers/Home.js
+++ Frontend/src/containers/Home.js
@@ -1,5 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Home = () => <div>Home</div>;
+const Home = () => (
+    <div className="container ">
+        <div className="jumbotron mt-5">
+            <h1 className="display-4">Well Come to Training page</h1>
+            <p className="lead">
+                This is an incredible authentication system with production
+                level feature!
+            </p>
+            <hr className="my-4" />
+            <p>Click the Log In button</p>
+            <p className="lead">
+                <Link
+                    className="btn btn-primary btn-lg"
+                    to="/login"
+                    role="button"
+                >
+                    Login
+                </Link>
+            </p>
+        </div>
+    </div>
+);
 
 export default Home;
